0a9691d26ec75f1ff9f89bb91e6db1a42c902e8d
Retus1n <retusin80@gmail.com>
2023-05-09 19:30:54 +0300

    feat('Header styles')

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 
 export const Header: FC<HeaderProps> = ({ currentUser }) => (
   <div className='fixed w-full bg-white  z-20 shadow-sm '>
-    <div className='py-4 border-b-[1px]'>
+    <div className='py-4 border-b-[1px] border-b-bgPrimary'>
       <Container>
         <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
           <Logo />
